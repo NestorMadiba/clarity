6f51e8721d4f989f40b18ce03e9ef2f16ef716cb
NestorMadiba <madibasystem@gmail.com>
2026-06-05 18:13:10 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// ============================================================
+// CLARITY — CONFIGURACIÓN
+// ============================================================
+
+const SUPABASE_URL      = 'https://nkuxstcilmbtvqradsdj.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5rdXhzdGNpbG1idHZxcmFkc2RqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2MzUyMjgsImV4cCI6MjA5NDIxMTIyOH0.lxfvYZ8Q1sImz-Xbmm7Kxcs_ODLwGvHJIVZXXLl30uw';
